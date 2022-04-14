9e155bc65c0b73954450f9431234df40270f7c73
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-04-14 17:19:01 -0600

    Create fair documents component

---
+++ src/app/components/administrator/dashboard/fair-documents/fair-documents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fair-documents',
+  templateUrl: './fair-documents.component.html',
+  styleUrls: ['./fair-documents.component.css']
+})
+export class FairDocumentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
