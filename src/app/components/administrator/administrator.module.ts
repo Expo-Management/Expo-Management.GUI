635915b9ad19d4b405468f7bbdb88f7a0a115f8b
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-04-14 17:06:29 -0600

    Change the structure of the administrator module

---
+++ src/app/components/administrator/administrator.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContentComponent } from './dashboard/content/content.component';
+import { TopMenuComponent } from './dashboard/top-menu/top-menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SideBarComponent } from './dashboard/side-bar/side-bar.component';
+import { MaterialsModule } from 'src/app/shared/materials/materials.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AdministratorComponent } from './administrator.component';
+import { FairDocumentsComponent } from './dashboard/fair-documents/fair-documents.component';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    TopMenuComponent,
+    SideBarComponent,
+    ContentComponent,
+    AdministratorComponent,
+    FairDocumentsComponent,
+  ],
+  imports: [
+    ReactiveFormsModule,
+    HttpClientModule,
+    MaterialsModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { 
+        path: '', 
+        component: AdministratorComponent,
+        children: [
+          { path: 'home', component: ContentComponent },
+          { path: 'documentation', component: FairDocumentsComponent },
+          { path: '**', component: ContentComponent }
+        ]
+      },
+    ]),
+  ]
+})
+export class AdministratorModule { }
