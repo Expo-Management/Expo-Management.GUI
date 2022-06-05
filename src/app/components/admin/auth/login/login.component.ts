d094bc93062e29000ca81b3ce4750b3ba83ac0a6
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-06-04 18:01:34 -0600

    Enable the back-end calls for the authentication in the login component.

--- src/app/components/admin/auth/login/login.component.ts
+++ src/app/components/admin/auth/login/login.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-// import { CustomPopUpService } from 'src/app/shared/custom-pop-up/custom-pop-up.service';
-// import { AuthService } from 'src/app/shared/services/auth.service';
-// import { PersonalInformationService } from 'src/app/shared/services/personal-information.service';
-// import { TokenStorageService } from 'src/app/shared/services/token-storage.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { CustomPopUpService } from 'src/app/shared/services/custom-pop-up.service';
+import { PersonalInformationService } from 'src/app/shared/services/personal-information.service';
+import { TokenStorageService } from 'src/app/shared/services/token-storage.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   loginForm = new FormGroup({
     username: new FormControl('', {
@@ -24,50 +24,58 @@ export class LoginComponent {
   });
 
   constructor(
-    // private authService: AuthService,
-    // private token: TokenStorageService,
-    // private user_info: PersonalInformationService,
-    // private customPopUpService: CustomPopUpService
+    private authService: AuthService,
+    private token: TokenStorageService,
+    private user_info: PersonalInformationService,
+    private customPopUpService: CustomPopUpService
   ) {}
 
-  // openCustomPopUp(message: string) {
-  //   this.customPopUpService.confirm(
-  //     'User login', 
-  //     message,
-  //     'auth/login'
-  //     );
-  // }
+  openCustomPopUp(message: string) {
+    this.customPopUpService.confirm(
+      'User login', 
+      message,
+      'auth/login'
+      );
+  }
 
   isLoggedIn = false;
   isLoginFailed = false;
   errorMessage = '';
 
   onSubmit() {
-    // this.authService.login(
-    //   this.loginForm.controls['username'].value,
-    //   this.loginForm.controls['password'].value).subscribe(
-    //     data => {
-    //       this.token.saveToken(data.token);
-    //       this.user_info.savePersonalInfo(
-    //         data.id,
-    //         data.userName, 
-    //         data.userEmail,
-    //         data.userFirst, 
-    //         data.userLast
-    //       )
-    //       this.isLoginFailed = false;
-    //       this.isLoggedIn = true;
-    //       this.reloadPage();
-    //     },
-    //     err => {
-    //       this.errorMessage = err.error.message;
-    //       this.openCustomPopUp(this.errorMessage);
-    //       this.isLoginFailed = true;
-    //     }
-    //   );
+    this.authService.login(
+      this.loginForm.controls['username'].value,
+      this.loginForm.controls['password'].value).subscribe(
+        data => {
+          console.log(data)
+          this.token.saveToken(data.token);
+          this.user_info.saveRole(
+            data.role
+          )
+          this.isLoginFailed = false;
+          this.isLoggedIn = true;
+          this.reloadPage();
+        },
+        err => {
+          console.log(err)
+          if (err.status === 404) {
+            this.errorMessage = 'No server found!';
+            this.openCustomPopUp('No server found!');
+            this.isLoginFailed = true;
+          } else {
+            this.errorMessage = err.error.message;
+            this.openCustomPopUp(this.errorMessage);
+            this.isLoginFailed = true;
+          }
+        }
+      );
   }
 
   reloadPage() {
     window.location.reload();
+  }
+
+  ngOnInit() {
+    console.log(localStorage);
   }
 }
