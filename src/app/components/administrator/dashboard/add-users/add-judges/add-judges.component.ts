895d2b47de38dfd4004eb29f662fdbce9392220f
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-06-06 11:04:29 -0600

    Enable the creation of judges for the judge module of administrator.

--- src/app/components/administrator/dashboard/add-users/add-judges/add-judges.component.ts
+++ src/app/components/administrator/dashboard/add-users/add-judges/add-judges.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
+import { CustomPopUpService } from 'src/app/shared/services/custom-pop-up.service';
+import { JudgesService } from 'src/app/shared/services/judges.service';
 
 //error validations when sumitted
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -18,16 +20,82 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 
 export class AddJudgesComponent implements OnInit {
-  NameFormControl = new FormControl('', [Validators.required, Validators.required]);
-  LastFormControl = new FormControl('', [Validators.required, Validators.required]);
-  EmailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  PasswordFormControl = new FormControl('', [Validators.required, Validators.required]);
-  IdFormControl = new FormControl('', [Validators.required, Validators.required]);
 
-  matcher = new MyErrorStateMatcher();
-  constructor() { }
+  createJudgeForm = new FormGroup({
+    NameFormControl: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+    LastFormControl: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+    UsernameFormControl: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+    PhoneFormControl: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+    EmailFormControl: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+    PasswordFormControl: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+    IdFormControl: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+  })
+
+  constructor(
+    private judgesService: JudgesService,
+    private customPopUpService: CustomPopUpService
+  ) { }
+
+  openCustomPopUp(message: string) {
+    this.customPopUpService.confirm(
+      'Judge creation', 
+      message,
+      'administrator/manage-judges'
+      );
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.judgesService.addJudge(
+      this.createJudgeForm.controls['IdFormControl'].value,
+      this.createJudgeForm.controls['UsernameFormControl'].value,
+      this.createJudgeForm.controls['NameFormControl'].value,
+      this.createJudgeForm.controls['LastFormControl'].value,
+      this.createJudgeForm.controls['EmailFormControl'].value,
+      this.createJudgeForm.controls['PhoneFormControl'].value,
+      this.createJudgeForm.controls['PasswordFormControl'].value,
+    ).subscribe(
+      data => {
+        this.openCustomPopUp('Judge created!');
+      },
+      err => {
+        if (err.status === 200) {
+          this.openCustomPopUp('Judge created!');
+        } else {
+          this.openCustomPopUp('There was a problem creating the judge, check the information or try again later.');
+        }
+      }
+    );
   }
 
 }
