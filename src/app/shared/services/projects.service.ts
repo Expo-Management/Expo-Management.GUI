eb66bbf74496223e7b44f1f7086d00862f524b4b
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-07-25 17:37:32 -0600

    Fix errors

--- src/app/shared/services/projects.service.ts
+++ src/app/shared/services/projects.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ProjectQualifications } from '../interfaces/project-qualifications';
 
 
 const httpOptions = {
