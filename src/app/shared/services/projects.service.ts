451e2f18d9b17e5c64fa5515d06433ff6dbd286a
jmo.py <33186474+JafetMo5601@users.noreply.github.com>
2022-07-25 17:36:38 -0600

    Update projects.service.ts

--- src/app/shared/services/projects.service.ts
+++ src/app/shared/services/projects.service.ts
@@ -22,27 +22,33 @@ export class ProjectsService {
   constructor(private httpClient: HttpClient) { }
 
   getCurrentProjects(): Observable<any> {
-    return this.httpClient.get('/api/Projects/current-fair-projects', httpOptions);
+    return this.httpClient.get(environment.apiUrl + '/Projects/current-fair-projects', httpOptions);
   }
 
   getOlProjects(): Observable<any> {
-    return this.httpClient.get('/api/Projects/old-projects', httpOptions);
+    return this.httpClient.get(environment.apiUrl + '/Projects/old-projects', httpOptions);
   }
 
   getMentions(): Observable<any> {
-    return this.httpClient.get('/api/Projects/mentions', httpOptions);
+    return this.httpClient.get(environment.apiUrl + '/Projects/mentions', httpOptions);
   }
 
   ShowProjects(): Observable<any>{
-    return this.httpClient.get('/api/Projects/projects', httpOptions)
+    return this.httpClient.get(environment.apiUrl + '/Projects/projects', httpOptions)
   }
 
-  CreateProject
-  (
-    formData: FormData
-  ){
-    return this.httpClient.post('/api/Projects/projects', formData)
+  CreateProject(formData: FormData){
+    return this.httpClient.post(environment.apiUrl + '/Projects/projects', formData)
   }
 
-  
+  getProjectDetails(projectId: string | null): Observable<ProjectQualifications[]>{
+    return this.httpClient.get<ProjectQualifications[]>(environment.apiUrl + `/Projects/project?projectId=${projectId}`, httpOptions)
+  }
+
+  createProjectClaim(projectId: string | null, claimDescription: string): Observable<any>{
+    return this.httpClient.post(
+      environment.apiUrl + '/Projects/create-claim', 
+      {projectId, claimDescription}, 
+      httpOptions)
+  }
 }
