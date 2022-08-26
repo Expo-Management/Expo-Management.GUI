99047581283836b9a826b498670cebca1ee3f811
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-08-25 20:34:58 -0600

    Implement charts in the dashboard.

--- src/app/components/administrator/dashboard/content/content.component.ts
+++ src/app/components/administrator/dashboard/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from 'src/app/shared/services/projects.service';
 
 @Component({
   selector: 'app-content',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  
-  constructor() { }
+  projectsByYear = []
+  projectsByCategory = []
+  projectsByQualifications = []
+  usersPerProject = []
+
+  constructor(
+    private projectServices: ProjectsService
+  ) { }
 
   ngOnInit(): void {
+    this.projectServices.getProjectsByYear().subscribe(
+      data => {
+        this.projectsByYear = data
+      }
+    )
+
+    this.projectServices.getProjectsByCategory().subscribe(
+      data => {
+        this.projectsByCategory = data
+      }
+    )
+
+    this.projectServices.getUsersByProject().subscribe(
+      data => {
+        this.usersPerProject = data
+        console.log(this.usersPerProject)
+      }
+    )
+
+    this.projectServices.getProjectsByQualifications().subscribe(
+      data => {
+        this.projectsByQualifications = data
+      }
+    )
   }
 
 }
