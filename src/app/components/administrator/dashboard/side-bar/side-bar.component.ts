d472ac41cd8f011749fa9615d7ec9a59ef8d5e1f
PC-ANDRES\andre <andreslml.ab@gmail.com>
2022-09-26 18:07:55 -0600

    Restore side and nav bars

--- src/app/components/administrator/dashboard/side-bar/side-bar.component.ts
+++ src/app/components/administrator/dashboard/side-bar/side-bar.component.ts
@@ -1,46 +1,59 @@
-import { Component, ViewChild } from '@angular/core';
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { MatSidenav } from '@angular/material/sidenav';
-import { delay, filter } from 'rxjs/operators';
-import { NavigationEnd, Router } from '@angular/router';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
-export class SideBarComponent{
+export class SideBarComponent implements OnInit {
 
-  @ViewChild(MatSidenav)
-  sidenav!: MatSidenav;
+  user_options = [
+    { 
+      icon: 'supervised_user_circle',
+      text: 'Profesores',
+      link: 'manage-professors',
+    },
+    { 
+      icon: 'assignment_ind',
+      text: 'Jueces',
+      link: 'manage-judges',
+    },
+    { 
+      icon: 'school',
+      text: 'Estudiantes',
+      link: 'manage-students',
+    },
+  ]
 
-  constructor(private observer: BreakpointObserver, private router: Router) {}
+  fair_options = [
+    { 
+      icon: 'assignment',
+      text: 'Documentos de la feria',
+      link: 'fair-documents',
+    },
+    { 
+      icon: 'calendar_today',
+      text: 'Calendario de la feria',
+      link: 'fair-calendar',
+    },
+  ]
 
-  ngAfterViewInit() {
-    this.observer
-      .observe(['(max-width: 800px)'])
-      .pipe(delay(1), untilDestroyed(this))
-      .subscribe((res: any) => {
-        if (res.matches) {
-          this.sidenav.mode = 'over';
-          this.sidenav.close();
-        } else {
-          this.sidenav.mode = 'side';
-          this.sidenav.open();
-        }
-      });
+  general_options = [
+    { 
+      icon: 'home',
+      text: 'Principal',
+      link: 'content',
+    },
+    { 
+      icon: 'info',
+      text: 'Bitacora del sistema',
+      link: 'logs',
+    }
+  ]
 
-    this.router.events
-      .pipe(
-        untilDestroyed(this),
-        filter((e) => e instanceof NavigationEnd)
-      )
-      .subscribe(() => {
-        if (this.sidenav.mode === 'over') {
-          this.sidenav.close();
-        }
-      });
+  constructor() { }
+
+  ngOnInit(): void {
   }
 
 }
