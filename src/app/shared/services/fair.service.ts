6aa6f30d295d304962877c2d0951007e9f770d73
LAPTOP-BP5TAFDG\valek <valek2920@gmail.com>
2022-09-29 15:17:15 -0600

    delete mongodb import

--- src/app/shared/services/fair.service.ts
+++ src/app/shared/services/fair.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ConnectionClosedEvent } from 'mongodb';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
