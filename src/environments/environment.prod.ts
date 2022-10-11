a75e96ea36315c00a579206880f101d3b696c0dd
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-10-10 19:37:30 -0600

    Change the prod environment url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://expomanagementapi2.azurewebsites.net/api',
+  apiUrl: 'https://expo-management-api.azurewebsites.net/api',
   siteKey: '6LfNLP0hAAAAAB7-0U_-Nq3cjyC5QFMs6EnfjASK'
 };
